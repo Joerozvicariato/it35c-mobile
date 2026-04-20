daf658f872369aba7e221e62726ad005673db1ad
Joerovicariato <Joerovicariato287@gmail.com>
2026-04-20 14:59:18 +0800

    hooks

--- src/pages/home-tabs/Feed.tsx
+++ src/pages/home-tabs/Feed.tsx
@@ -1,8 +1,19 @@
-import { IonButtons, IonContent, IonHeader, IonMenuButton, IonPage, IonTitle } from "@ionic/react";
+import React from "react";
+import { IonButtons, IonContent, IonHeader, IonMenuButton, IonPage, IonTitle, IonList, IonItem, IonLabel } from "@ionic/react";
 
 
 const Favorite: React.FC = () => {
-  return (
+
+  const games = [
+    {name: 'Pokemon Yellow'},
+    {name: 'Mega man X '},
+    {name: 'The legend of zelda'},
+    {name: 'Pacman  '},
+    {name: 'Super Mario World  '},
+    {name: 'Bomberman'},
+  ]
+
+    return (
     <IonPage>
         <IonHeader>
             <IonButtons>
@@ -12,9 +23,19 @@ const Favorite: React.FC = () => {
         </IonHeader>
         <IonContent className="ion-padding">
             <h1>Favorite</h1>
+
+
+            <IonList>
+                {games.map((game, index) => (
+                    <IonItem key={index}>
+                        <IonLabel>{game.name}</IonLabel>
+                    </IonItem>
+                ))}
+            </IonList>
         </IonContent>
     </IonPage>
   )
 };
+
 
 export default Favorite;
