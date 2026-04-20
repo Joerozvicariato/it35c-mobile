3165a357106f5ae69f7fafb6a0617820d36aff78
Joerovicariato <Joerovicariato287@gmail.com>
2026-04-20 19:42:24 +0800

    Added hooks

--- src/pages/home-tabs/Feed.tsx
+++ src/pages/home-tabs/Feed.tsx
@@ -1,8 +1,9 @@
 import React from "react";
-import { IonButtons, IonContent, IonHeader, IonMenuButton, IonPage, IonTitle, IonList, IonItem, IonLabel } from "@ionic/react";
+import { IonButtons, IonContent, IonHeader, IonMenuButton, IonPage, IonTitle, IonList, IonItem, IonLabel, IonCard, IonCardTitle, IonButton, IonIcon, IonGrid, IonCardContent, IonCardSubtitle, IonCardHeader, IonCol, IonRow, IonSearchbar } from "@ionic/react";
+import { chatboxEllipsesOutline, search, shareOutline, thumbsUp, thumbsUpOutline } from "ionicons/icons";
 
 
-const Favorite: React.FC = () => {
+const Feed: React.FC = () => {
 
   const games = [
     {name: 'Pokemon Yellow'},
@@ -12,30 +13,54 @@ const Favorite: React.FC = () => {
     {name: 'Super Mario World  '},
     {name: 'Bomberman'},
   ]
-
-    return (
+  
+return (
     <IonPage>
         <IonHeader>
             <IonButtons>
                 <IonMenuButton></IonMenuButton>
-                <IonTitle>Favorite</IonTitle>
+                <IonTitle>Feed</IonTitle>
             </IonButtons>
         </IonHeader>
         <IonContent className="ion-padding">
-            <h1>Favorite</h1>
-
 
             <IonList>
-                {games.map((item, index) => (
-                    <IonItem key={index}>
-                        <IonLabel>{item.name}</IonLabel>
-                    </IonItem>
-                ))}
-            </IonList>
+                    {games.map(( item, index) => (
+                        <IonCard key={index}>
+                            <img alt="Silhouette of mountains" src="https://ionicframework.com/docs/img/demos/card-media.png" />
+                            <IonCardHeader>
+                                <IonCardTitle>{item.name}</IonCardTitle>
+                                <IonCardSubtitle>Card Subtitle</IonCardSubtitle>
+                            </IonCardHeader>
+                            <IonCardContent>Here's a small text description for the card content. Nothing more, nothing less.</IonCardContent>
+                            <IonGrid>
+                                <IonRow>
+                                    <IonCol>
+                                        <IonButton fill="clear" expand ='full'>
+                                            <IonIcon icon={thumbsUpOutline}></IonIcon>
+                                            <IonLabel style={{ marginLeft: '5px' }}>Like</IonLabel>
+                                        </IonButton>
+                                    </IonCol>
+                                    <IonCol>
+                                        <IonButton fill="clear" expand ='full'>
+                                            <IonIcon icon={chatboxEllipsesOutline}></IonIcon>
+                                            <IonLabel style={{ marginLeft: '5px' }}>Comment</IonLabel>
+                                        </IonButton>
+                                    </IonCol>
+                                    <IonCol>
+                                        <IonButton fill="clear" expand ='full'>
+                                            <IonIcon icon={shareOutline}></IonIcon>
+                                            <IonLabel style={{ marginLeft: '5px' }}>Share</IonLabel>
+                                        </IonButton>
+                                    </IonCol>
+                                </IonRow>
+                            </IonGrid>
+                        </IonCard>
+                    ))}
+                </IonList>
         </IonContent>
     </IonPage>
-  )
+
+  );
 };
-
-
-export default Favorite;
+export default Feed;
